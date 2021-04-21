e48402156f7d0128a7b6156276940a2612b42979
João Mandaglio <jhmandaglio@gmail.com>
2021-04-21 12:53:44 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/mandaglio/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.mandaglio.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.mandaglio.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
