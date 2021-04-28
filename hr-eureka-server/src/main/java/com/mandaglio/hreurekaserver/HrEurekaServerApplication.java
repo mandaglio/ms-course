461e64adbdd43dcc0e428e15c01fddb8c36649f7
João Mandaglio <jhmandaglio@gmail.com>
2021-04-28 19:12:29 -0300

    resolvendo nome eureka

--- hr-eureka-server/src/main/java/com/mandaglio/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/mandaglio/hreurekaserver/HrEurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class HrEurecaServerApplication {
+public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrEurecaServerApplication.class, args);
+		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
 
 }
