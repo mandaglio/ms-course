9697f6609fc356bf065b6ae5aa16d4e7e6b73164
João Mandaglio <jhmandaglio@gmail.com>
2021-04-28 23:33:02 -0300

    hystrix fault tolerance

--- hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
@@ -43,6 +43,14 @@ public class WorkerResources {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+// Teste para ativar o hystrix		
+//		try {
+//			Thread.sleep(3000L);
+//		}catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}	
+		
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
