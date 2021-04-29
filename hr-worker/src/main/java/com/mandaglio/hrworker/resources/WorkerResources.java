9c3b942050fb021034b0fd0cfe983abd8d9a2390
João Mandaglio <jhmandaglio@gmail.com>
2021-04-29 18:16:59 -0300

    actuator for runtime config refresh

--- hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mandaglio.hrworker.entities.Worker;
 import com.mandaglio.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
