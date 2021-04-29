82778f988bdef510812f9522385f93410327094a
João Mandaglio <jhmandaglio@gmail.com>
2021-04-29 18:08:40 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/mandaglio/hrworker/resources/WorkerResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,11 +24,20 @@ public class WorkerResources {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
@@ -45,11 +55,6 @@ public class WorkerResources {
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 
 // Teste para ativar o hystrix		
-		try {
-			Thread.sleep(3000L);
-		}catch (InterruptedException e) {
-			e.printStackTrace();
-		}	
 		
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
